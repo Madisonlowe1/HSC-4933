f1c6a71ac400d123027489e8a3b1ae58ac8d2a5e
Madisonlowe1 <mlowenstein@usf.edu>
2026-08-26 10:42:41 -0400

    Add main.py via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+###########################################
+# Madison Lowenstein # mlowenstein@usf.edu #
+#-----------------------------------------#
+# main.py: Prints "Hello, World!" and #
+# examins behavior of data types.  #
+#############################
+"""We want this script to PRINT the string HELLO, World!"""
+
+print("Hello, World!")
+
+
+a= "hello world!"
+print(a)
